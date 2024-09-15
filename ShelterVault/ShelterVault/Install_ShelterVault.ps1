92322f7bf91957d8ffc3882354d05d8d823766dd
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-09-14 22:53:45 -0600

    Add workaround script

--- ShelterVault/ShelterVault/Install_ShelterVault.ps1
+++
@@ -1,5 +0,0 @@
-﻿Write-Output 'Fixing Add-AppDevPackage.ps1...'
-(Get-Content .\Add-AppDevPackage.ps1).Replace('$DependencyPackages = @()', '[string[]]$DependencyPackages = @()') | Set-Content
-.\Add-AppDevPackage.ps1
-Write-Output 'Installing ShelterVault...'
-.\Install.ps1
