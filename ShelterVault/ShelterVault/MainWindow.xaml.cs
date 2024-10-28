9d003493d621b578914b7d86afca48d0d0cd18fe
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-10-27 23:44:49 -0600

    Refactored navigation for MainWindow, MainPage, SettingsPage and CredentialsPage

--- ShelterVault/ShelterVault/MainWindow.xaml.cs
+++ ShelterVault/ShelterVault/MainWindow.xaml.cs
@@ -21,32 +21,5 @@ namespace ShelterVault
             this.ExtendsContentIntoTitleBar = true;
             this.SetTitleBar(AppTitleBar);
         }
-
-        private void NavigationView_SelectionChanged8(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
-        {
-            /* NOTE: for this function to work, every NavigationView must follow the same naming convention: nvSample# (i.e. nvSample3),
-            and every corresponding content frame must follow the same naming convention: contentFrame# (i.e. contentFrame3) */
-
-            // Get the sample number
-            string sampleNum = (sender.Name).Substring(8);
-            Debug.Print("num: " + sampleNum + "\n");
-
-            if (args.IsSettingsSelected)
-            {
-                this.contentFrame8.Navigate(typeof(SettingsPage));
-            }
-            else
-            {
-                if (args.SelectedItem == null) return;
-                var selectedItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)args.SelectedItem;
-                string selectedItemTag = ((string)selectedItem.Tag);
-                if (string.IsNullOrWhiteSpace(selectedItemTag)) return;
-                sender.Header = "Sample Page " + selectedItemTag.Substring(selectedItemTag.Length - 1);
-                string pageName = "ShelterVault.Views." + selectedItemTag;
-                Type pageType = Type.GetType(pageName);
-                if (pageType == null) return;
-                this.contentFrame8.Navigate(pageType);
-            }
-        }
     }
 }
