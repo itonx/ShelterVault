5922e75052ec5487718a29da6e1b91f9d425f81d
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-10-30 03:31:06 -0600

    Select menu items by tag object.

---
+++ ShelterVault/ShelterVault/Shared/Converters/TagObjectToItemConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelterVault.Shared.Converters
+{
+    public class TagObjectToItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShelterVault/ShelterVault/Shared/Messages/SelectCredentialRequestMessage.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using ShelterVault.Models;
+using ShelterVault.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelterVault.Shared.Messages
+{
+    public class SelectCredentialRequestMessage : ValueChangedMessage<Credential>
+    {
+        public SelectCredentialRequestMessage(Credential value) : base(value)
+        {
+        }
+    }
+}
