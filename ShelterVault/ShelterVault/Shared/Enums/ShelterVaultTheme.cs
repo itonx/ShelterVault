d6321299605a2f82f97ae1c10a27811e03cebc68
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-11-01 03:15:00 -0600

    Add icons for theme button changer.

--- ShelterVault/ShelterVault/Shared/Enums/ShelterVaultTheme.cs
+++ ShelterVault/ShelterVault/Shared/Enums/ShelterVaultTheme.cs
@@ -9,11 +9,11 @@ namespace ShelterVault.Shared.Enums
 {
     public enum ShelterVaultTheme
     {
-        [ThemeStyle(Microsoft.UI.Xaml.ElementTheme.Light, "OverrideDefaultTheme")]
+        [ThemeStyle(Microsoft.UI.Xaml.ElementTheme.Light, "OverrideDefaultTheme", "\uE793")]
         LIGHT,
-        [ThemeStyle(Microsoft.UI.Xaml.ElementTheme.Dark, "OverrideDefaultTheme")]
+        [ThemeStyle(Microsoft.UI.Xaml.ElementTheme.Dark, "OverrideDefaultTheme", "\uF0CE")]
         DARK,
-        [ThemeStyle(Microsoft.UI.Xaml.ElementTheme.Dark, "NeuromancerTheme")]
+        [ThemeStyle(Microsoft.UI.Xaml.ElementTheme.Dark, "NeuromancerTheme", "\uE950")]
         NEUROMANCER,
     }
 }
