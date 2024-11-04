7b2a2bf9c0f508489d4897bd824e288c41279356
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-11-03 21:33:09 -0600

    Refactor password bindings.

---
+++ ShelterVault/ShelterVault/Shared/Extensions/PasswordBoxExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using ShelterVault.Shared.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ShelterVault.Shared.Extensions
+{
+    public static class PasswordBoxExtensions
+    {
+        public static void ExecuteAttachedCommand(this ExecuteCommandOnPasswordChangedBehavior passwordBoxBehavior, DependencyProperty dependencyProperty)
+        {
+            passwordBoxBehavior?.Command?.Execute(passwordBoxBehavior.AssociatedObject?.Password ?? null);
+        }
+    }
+}
