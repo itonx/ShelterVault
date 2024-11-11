4d4337f7e017893272066ca65cf59eb926c7c20c
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-11-11 02:13:27 -0600

    Add multi-language support to dialog service.

--- ShelterVault/ShelterVault/Services/DialogService.cs
+++ ShelterVault/ShelterVault/Services/DialogService.cs
@@ -13,27 +13,38 @@ namespace ShelterVault.Services
 {
     internal interface IDialogService
     {
-        Task ShowConfirmationDialogAsync(string title, string message, string primaryButtonText = "Close");
-        Task<bool> ShowContinueConfirmationDialogAsync(string title, string message, string primaryButtonText = "No", string secondaryButtonText = "Yes", ContentDialogResult expectedResult = ContentDialogResult.Primary);
+        Task ShowConfirmationDialogAsync(string messageResourceKey, string titleResourceKey = LangResourceKeys.DIALOG_TITLE_DEFAULT, string primaryButtonTextResourceKey = LangResourceKeys.DIALOG_CLOSE_DEFAULT);
+        Task<bool> ShowContinueConfirmationDialogAsync(string messageResourceKey, string titleResourceKey = LangResourceKeys.DIALOG_TITLE_DEFAULT, string primaryButtonTextResourceKey = LangResourceKeys.DIALOG_CLOSE_NO, string secondaryButtonResourceKey = LangResourceKeys.DIALOG_CLOSE_YES, ContentDialogResult expectedResult = ContentDialogResult.Primary);
     }
     internal class DialogService : IDialogService
     {
-        public async Task ShowConfirmationDialogAsync(string title, string message, string primaryButtonText = "Close")
+        private readonly ILanguageService _languageService;
+        
+        public DialogService(ILanguageService languageService)
         {
-            ContentDialog dialog = BuildDialog(title, message, primaryButtonText);
+            _languageService = languageService;
+        }
+
+        public async Task ShowConfirmationDialogAsync(string messageResourceKey, string titleResourceKey = LangResourceKeys.DIALOG_TITLE_DEFAULT, string primaryButtonTextResourceKey = LangResourceKeys.DIALOG_CLOSE_DEFAULT)
+        {
+            ContentDialog dialog = BuildDialog(titleResourceKey, messageResourceKey, primaryButtonTextResourceKey);
             await dialog.ShowAsync();
         }
 
-        public async Task<bool> ShowContinueConfirmationDialogAsync(string title, string message, string primaryButtonText = "No", string secondaryButtonText = "Yes", ContentDialogResult expectedResult = ContentDialogResult.Primary)
+        public async Task<bool> ShowContinueConfirmationDialogAsync(string messageResourceKey, string titleResourceKey = LangResourceKeys.DIALOG_TITLE_DEFAULT, string primaryButtonTextResourceKey = LangResourceKeys.DIALOG_CLOSE_NO, string secondaryButtonResourceKey = LangResourceKeys.DIALOG_CLOSE_YES, ContentDialogResult expectedResult = ContentDialogResult.Primary)
         {
-            ContentDialog dialog = BuildDialog(title, message, primaryButtonText);
-            dialog.SecondaryButtonText = secondaryButtonText;
+            ContentDialog dialog = BuildDialog(titleResourceKey, messageResourceKey, primaryButtonTextResourceKey);
+            dialog.SecondaryButtonText = _languageService.GetLangValue(secondaryButtonResourceKey);
             ContentDialogResult result = await dialog.ShowAsync();
             return result == expectedResult;
         }
 
-        private ContentDialog BuildDialog(string title, string message, string primaryButtonText = "Close")
+        private ContentDialog BuildDialog(string titleResourceKey, string messageResourceKey, string primaryButtonTextResourceKey = LangResourceKeys.DIALOG_CLOSE_DEFAULT)
         {
+            string title = _languageService.GetLangValue(titleResourceKey);
+            string message = _languageService.GetLangValue(messageResourceKey);
+            string primaryButtonText = _languageService.GetLangValue(primaryButtonTextResourceKey);
+
             ContentDialog dialog = new ContentDialog();
             dialog.XamlRoot = WindowHelper.CurrentMainWindow.Content.XamlRoot;
             dialog.RequestedTheme = (WindowHelper.CurrentMainWindow.Content as FrameworkElement).RequestedTheme;
--- ShelterVault/ShelterVault/Shared/Constants/LangResourceKeys.cs
+++ ShelterVault/ShelterVault/Shared/Constants/LangResourceKeys.cs
@@ -14,5 +14,21 @@ namespace ShelterVault.Shared.Constants
         public const string ENGLISH_OPTION = "SelectEnglishLang/Content";
         public const string SPANISH_OPTION = "SelectSpanishLang/Content";
         public const string SWITCH_VAULT = "AppBarSwitchVault/Text";
+
+        public const string DIALOG_TITLE_DEFAULT = "DialogTitleDefault/Text";
+        public const string DIALOG_CLOSE_DEFAULT = "DialogCloseDefault/Text";
+        public const string DIALOG_CLOSE_NO = "DialogCloseNo/Text";
+        public const string DIALOG_CLOSE_YES = "DialogCloseYes/Text";
+        public const string DIALOG_WRONG_MASTER_KEY = "DialogWrongMasterKey/Text";
+        public const string DIALOG_CREDENTIALS_DELETED = "DialogCredentialsDeleted/Text";
+        public const string DIALOG_CREDENTIALS_NOT_DELETED = "DialogCredentialsNotDeleted/Text";
+        public const string DIALOG_CREDENTIALS_UPDATED = "DialogCredentialsUpdated/Text";
+        public const string DIALOG_CREDENTIALS_NOT_UPDATED = "DialogCredentialsNotUpdated/Text";
+        public const string DIALOG_CREDENTIALS_SAVED = "DialogCredentialsSaved/Text";
+        public const string DIALOG_CREDENTIALS_NOT_SAVED = "DialogCredentialsNotSaved/Text";
+        public const string DIALOG_CREDENTIALS_EMPTY_TITLE = "DialogCredentialsEmptyTitle/Text";
+        public const string DIALOG_CREDENTIALS_PASSWORD_MINIMUM_REQUIREMENTS_ERROR = "DialogCredentialsPasswordMinimumRequirementsError/Text";
+        public const string DIALOG_CREDENTIALS_PASSWORD_DO_NOT_MATCH = "DialogCredentialsPasswordsDoNotMatch/Text";
+        public const string DIALOG_CREDENTIALS_PENDING_CHANGES = "DialogCredentialsPendingChanges/Text";
     }
 }
