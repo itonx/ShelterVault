cd1a044ccae9ba06b0e4f83c434d4bf01a4a4fd3
Ayrton Teniente <ayrton.94.e@gmail.com>
2024-11-13 00:56:44 -0600

    Add code base for SettingsViewModel and SettingsService

--- ShelterVault/ShelterVault/Views/SettingsPage.xaml.cs
+++ ShelterVault/ShelterVault/Views/SettingsPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -5,6 +6,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using ShelterVault.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -26,6 +28,7 @@ namespace ShelterVault.Views
         public SettingsPage()
         {
             this.InitializeComponent();
+            this.DataContext = App.Current.Services.GetService<SettingsViewModel>();
         }
     }
 }
