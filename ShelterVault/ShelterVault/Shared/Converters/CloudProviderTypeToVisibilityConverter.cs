b237148730ce305c9321b6b94d138f66c6751ad7
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-02-20 23:00:10 -0600

    Fix visibility on cloud configuration form when cloud provider changes

---
+++ ShelterVault/ShelterVault/Shared/Converters/CloudProviderTypeToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using ShelterVault.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelterVault.Shared.Converters
+{
+    internal class CloudProviderTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is CloudProviderType cloudProviderType && parameter != null && int.TryParse(parameter.ToString(), out int selectedCloudProviderType) && cloudProviderType == (CloudProviderType)selectedCloudProviderType) 
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
