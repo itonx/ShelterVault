b862ba80b2c21c50ad4e410b566c0f9d86c4c5c3
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-03-03 23:23:36 -0600

    Add SheltreVault app title

---
+++ ShelterVault/ShelterVault/Shared/Converters/ShelterVaultAppStateToTitleMarginConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using ShelterVault.Shared.Enums;
+using System;
+
+namespace ShelterVault.Shared.Converters
+{
+    public class ShelterVaultAppStateToTitleMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is ShelterVaultAppState appState && appState == ShelterVaultAppState.NavigationView) return new Thickness(0);
+
+            return new Thickness(10, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
