18e699ff49d0e00286e1fdade580baed958b9df0
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-05-09 20:59:46 -0600

    Change custom title bar with WinUI3 built-in title bar.

---
+++ ShelterVault/ShelterVault/Shared/Behaviors/PaneToggleRequestedBehavior.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+using ShelterVault.Shared.Messages;
+
+namespace ShelterVault.Shared.Behaviors
+{
+    class PaneToggleRequestedBehavior : Behavior<TitleBar>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PaneToggleRequested += AssociatedObject_PaneToggleRequested;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PaneToggleRequested -= AssociatedObject_PaneToggleRequested;
+        }
+
+        private void AssociatedObject_PaneToggleRequested(TitleBar sender, object args)
+        {
+            WeakReferenceMessenger.Default.Send(new IsPaneOpenMessage(true));
+        }
+    }
+}
