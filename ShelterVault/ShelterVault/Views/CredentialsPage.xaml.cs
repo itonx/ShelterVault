2eb64f0f11fc7b58620fae1d3bd7074d388c1144
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-07-22 19:38:42 -0600

    Add link list to Credentials.

--- ShelterVault/ShelterVault/Views/CredentialsPage.xaml.cs
+++ ShelterVault/ShelterVault/Views/CredentialsPage.xaml.cs
@@ -1,20 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml.Controls;
 using ShelterVault.ViewModels;
- 
+
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -29,6 +16,15 @@ namespace ShelterVault.Views
         {
             this.InitializeComponent();
             this.DataContext = App.Current.Services.GetService<CredentialsViewModel>();
+        }
+
+        private void MenuFlyoutItem_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            var vm = this.DataContext as CredentialsViewModel;
+            if (vm != null)
+            {
+                vm.DeleteUrlCommand.Execute((sender as MenuFlyoutItem).Tag);
+            }
         }
     }
 }
