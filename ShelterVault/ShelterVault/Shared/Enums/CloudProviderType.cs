c58f57b1964cd6cfa5ea0d008ef86fb05e43e894
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-10-03 23:44:21 -0600

    Add cloud provider options translation for Spanish language

--- ShelterVault/ShelterVault/Shared/Enums/CloudProviderType.cs
+++ ShelterVault/ShelterVault/Shared/Enums/CloudProviderType.cs
@@ -1,8 +1,12 @@
-﻿namespace ShelterVault.Shared.Enums
+﻿using System.ComponentModel;
+
+namespace ShelterVault.Shared.Enums
 {
     public enum CloudProviderType
     {
+        [Description("CloudProviderType/None")]
         None,
+        [Description("CloudProviderType/Azure")]
         Azure
     }
 }
