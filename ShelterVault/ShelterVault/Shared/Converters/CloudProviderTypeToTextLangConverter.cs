78022cd386efd4057ebd4add3d58cdaecfe67dec
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-10-03 23:44:21 -0600

    Add cloud provider options translation for Spanish language

---
+++ ShelterVault/ShelterVault/Shared/Converters/CloudProviderTypeToTextLangConverter.cs
@@ -0,0 +1,25 @@
+﻿using Desktiny.UI.Extensions;
+using Desktiny.UI.Services;
+using Microsoft.UI.Xaml.Data;
+using ShelterVault.Shared.Enums;
+using System;
+using System.ComponentModel;
+
+namespace ShelterVault.Shared.Converters
+{
+    internal class CloudProviderTypeToTextLangConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is CloudProviderType cloudProviderType)
+                return LangService.GetLangValue(cloudProviderType.GetAttribute<DescriptionAttribute>()?.Description ?? string.Empty);
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
