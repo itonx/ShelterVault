59677565e87cb6b751c9c998615fced8ed2ecc56
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-11-23 01:49:33 -0600

    Add ComboBoxChangeLanguageBehavior from Desktiny.

--- ShelterVault/ShelterVault/ViewModels/MainWindowViewModel.cs
+++ ShelterVault/ShelterVault/ViewModels/MainWindowViewModel.cs
@@ -7,7 +7,6 @@ using ShelterVault.DataLayer;
 using ShelterVault.Managers;
 using ShelterVault.Models;
 using ShelterVault.Services;
-using ShelterVault.Shared.Constants;
 using ShelterVault.Shared.Enums;
 using ShelterVault.Shared.Messages;
 using System;
@@ -31,26 +30,18 @@ namespace ShelterVault.ViewModels
         [ObservableProperty]
         public partial bool ShowLangOptions { get; set; }
         [ObservableProperty]
-        public partial string EnglishLangOptionText { get; set; }
-        [ObservableProperty]
-        public partial string SpanishLangOptionText { get; set; }
-        [ObservableProperty]
-        public partial string SwitchVaultText { get; set; }
-        [ObservableProperty]
         public partial CloudSyncStatus CurrentCloudSyncStatus { get; set; }
 
         private readonly IShelterVaultThemeService _shelterVaultThemeService;
         private readonly IShelterVaultStateService _shelterVaultStateService;
-        private readonly ILanguageService _languageService;
         private readonly ICloudSyncManager _cloudSyncManager;
         private readonly IUIThreadService _uiThreadService;
         private readonly IWeakReferenceInstanceManager _weakReferenceInstanceManager;
 
-        public MainWindowViewModel(IShelterVault shelterVault, IShelterVaultThemeService shelterVaultThemeService, IShelterVaultStateService shelterVaultStateService, ILanguageService languageService, ICloudSyncManager cloudSyncManager, IUIThreadService uiThreadService, IWeakReferenceInstanceManager weakReferenceInstanceManager)
+        public MainWindowViewModel(IShelterVault shelterVault, IShelterVaultThemeService shelterVaultThemeService, IShelterVaultStateService shelterVaultStateService, ICloudSyncManager cloudSyncManager, IUIThreadService uiThreadService, IWeakReferenceInstanceManager weakReferenceInstanceManager)
         {
             _shelterVaultThemeService = shelterVaultThemeService;
             _shelterVaultStateService = shelterVaultStateService;
-            _languageService = languageService;
             _cloudSyncManager = cloudSyncManager;
             _uiThreadService = uiThreadService;
             _weakReferenceInstanceManager = weakReferenceInstanceManager;
@@ -88,7 +79,6 @@ namespace ShelterVault.ViewModels
             if (shelterVault.AreThereVaults()) ShelterVaultCurrentAppState = ShelterVaultAppState.ConfirmMasterKey;
             ShowLangOptions = true;
             ShowSwitchVault = false;
-            SetLangText();
             StartSynchronizationTask();
         }
 
@@ -109,10 +99,6 @@ namespace ShelterVault.ViewModels
                     ShowLangOptions = true;
                     ShowSwitchVault = false;
                 }
-            });
-            WeakReferenceMessenger.Default.Register<MainWindowViewModel, UpdateLanguageValuesMessage>(this, (viewModel, payload) =>
-            {
-                viewModel.SetLangText();
             });
             WeakReferenceMessenger.Default.Register<MainWindowViewModel, ProgressBarRequestMessage>(this, (viewModel, payload) =>
             {
@@ -142,16 +128,6 @@ namespace ShelterVault.ViewModels
                         viewModel.RefreshSyncStatusInfo();
                 });
             });
-        }
-
-        private void SetLangText()
-        {
-            EnglishLangOptionText = _languageService.GetLangValue(LangResourceKeys.ENGLISH_OPTION);
-            SpanishLangOptionText = _languageService.GetLangValue(LangResourceKeys.SPANISH_OPTION);
-            SwitchVaultText = _languageService.GetLangValue(LangResourceKeys.SWITCH_VAULT);
-            CloudSyncStatus tmpStatus = CurrentCloudSyncStatus;
-            CurrentCloudSyncStatus = CloudSyncStatus.None;
-            CurrentCloudSyncStatus = tmpStatus;
         }
 
         private void RefreshSyncStatusInfo()
